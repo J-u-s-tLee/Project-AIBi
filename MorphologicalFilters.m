3255910ed2a9137470350ce2a9d08ff05b681f06
mariarocha2310 <167978174+mariarocha2310@users.noreply.github.com>
2024-05-12 21:58:05 +0100

    Update MorphologicalFilters.m

--- MorphologicalFilters.m
+++ MorphologicalFilters.m
@@ -2,11 +2,11 @@ function [ROI] = MorphologicalFilters(InputImage)
 
 InputImage_gray = im2gray(InputImage);
 InputImage_gray_bin = imbinarize(InputImage_gray);
-SE = strel('square', 20);
+SE = strel('square', 21);
 InputImage_close = imclose(InputImage_gray_bin, SE);
-SE2 = strel('square', 50);
+SE2 = strel('square', 51);
 InputImage_erode = double(imerode(InputImage_close, SE2));
-SE3 = strel('square',40);
+SE3 = strel('square',41);
 InputImage_fill = imfill(InputImage_erode,"holes");
 ROI= imopen(InputImage_fill,SE3);
 
