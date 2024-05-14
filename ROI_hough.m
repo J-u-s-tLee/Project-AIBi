67383c1623a8ab68b1ca4178367f8b4eeb5935e1
J-u-s-tLee <154263742+J-u-s-tLee@users.noreply.github.com>
2024-05-14 22:25:29 +0100

    Update ROI_hough.m

--- ROI_hough.m
+++ ROI_hough.m
@@ -1,46 +1,31 @@
-%aplica o linear hough para detetar as linhas da imagem e assim defenir a
-%mascara a utilizar
-function mask = ROI_hough (I)
+function Mask = ROI_Hough(Image)
 
-%"processamento" da imagem (ver nome)
-Image_gray=im2gray(I);
-SE1 = strel('disk', 9); 
-Image_bottom = imbothat(Image_gray, SE1);
-T = graythresh(Image_bottom);
-Image_final = imbinarize(Image_bottom, T);
-
-%deteçao das 4 linhas limitantes
-[H, theta, rho] = hough(Image_final);
-P = houghpeaks(H,4);
-lines = houghlines(Image_final, theta, rho, P);
-% figure;
-% imshow(Image_gray);
-% hold on;
-% 
-% for k = 1:length(lines)
-% xy = [lines(k).point1; lines(k).point2];
-% plot(xy(:,1), xy(:,2), 'LineWidth', 2, 'Color', 'r');
-% end
-% 
-% hold off;
-% title('Linhas detetadas pela transformada de Hough');
-
-%criação da máscara
-background = Image_bottom;
-background(:,:)=0;
-
-for k = 1:length(lines)
-    xy = [lines(k).point1; lines(k).point2];
-    background = insertShape(background, 'Line', [xy(1,1) xy(1,2) xy(2,1) xy(2,2)], 'Color', 'white', 'LineWidth', 2);
-end
-
-box_gray = im2gray(background);
-box_bin = imbinarize(box_gray);
-box_filled = imfill(box_bin,'holes');
-SE2=strel('disk',3);
-mask = imopen(box_filled,SE2);
-% figure;
-% title('ROI final');
-% imshow(mask);
+    I = im2double(Image);
+    Image_gray = im2gray(I);
+    SE1 = strel('disk', 9); 
+    Image_bottom = imbothat(Image_gray, SE1);
+    T = graythresh(Image_bottom);
+    Image_final = imbinarize(Image_bottom, T);
+    
+    [H, theta, rho] = hough(Image_final);
+    P = houghpeaks(H,4);
+    lines = houghlines(Image_final, theta, rho, P);
+    
+    [m,n] = size(Image_bottom);
+    Background = zeros(m,n);
+    
+    for k = 1:length(lines)
+        for i = lines(k).point1(2):lines(k).point2(2)
+            for j = lines(k).point1(1):lines(k).point2(1)
+                Background(i,j) = 1;
+            end
+        end
+    end
+    
+    SE2 = strel('square', 200); 
+    Background_close = imclose(Background, SE2);
+    Background_fill = imfill(Background_close, 'holes');
+    SE3 = strel('square', 2); 
+    Mask = imopen(Background_fill, SE3);
 
 end
