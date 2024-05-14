d8324f2259fcad68db6e0235c780697526d8da33
J-u-s-tLee <154263742+J-u-s-tLee@users.noreply.github.com>
2024-05-14 22:23:42 +0100

    Update Preprocessing.m

--- Preprocessing.m
+++ Preprocessing.m
@@ -1,8 +1,11 @@
 function [Image_Out] = Preprocessing(Image_In)
+
+    Image_gray = im2gray(im2double(Image_In));
     SE1 = strel('disk', 50);
-    Image_bothat = imbothat(Image_In, SE1);
+    Image_bothat = imbothat(Image_gray, SE1);
     T = multithresh(Image_bothat, 7);
     Image_thresh = imquantize(Image_bothat, T);
     Image_thresh1 = Image_thresh <= 2;
     Image_Out = imcomplement(Image_thresh1);
+    
 end
