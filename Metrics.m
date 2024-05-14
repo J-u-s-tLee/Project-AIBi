ce55a0ad4eac4744eda437b12ffa92ebd7d5b404
J-u-s-tLee <154263742+J-u-s-tLee@users.noreply.github.com>
2024-05-14 22:24:17 +0100

    Update Metrics.m

--- Metrics.m
+++ Metrics.m
@@ -1,13 +1,15 @@
 function [Metrics_Table] = Metrics(Vetores_de_Imagens, Method)
 
 Max_TP = 0;
-Sum_TP = 0;
+Sum_TP = zeros(1,length(Vetores_de_Imagens{3}));
 Max_FP = 0;
-Sum_FP = 0;
+Sum_FP = zeros(1,length(Vetores_de_Imagens{3}));
 Max_FN = 0;
-Sum_FN = 0;
+Sum_FN = zeros(1,length(Vetores_de_Imagens{3}));
 Max_Jaccard = 0;
-Sum_Jaccard = 0;
+Sum_Jaccard = zeros(1,length(Vetores_de_Imagens{3}));
+Max_Euclidean = 0;
+Sum_Euclidean = zeros(1,length(Vetores_de_Imagens{3}));
 
 for i = 1: length(Vetores_de_Imagens{3})
     if(Method == "ROI_Hough")
@@ -19,10 +21,12 @@ for i = 1: length(Vetores_de_Imagens{3})
     ROI_GT = im2double(Vetores_de_Imagens{3}{1,i});
 
     [TP, FP, FN, Jaccard] = Evaluation(ROI, ROI_GT);
-    Sum_TP = Sum_TP + TP;
-    Sum_FP = Sum_FP + FP;
-    Sum_FN = Sum_FN + FN;
-    Sum_Jaccard = Sum_Jaccard + Jaccard;
+    [Mean_Eu, Max_Eu] = Euclidean_Distance(ROI, ROI_GT);
+    Sum_TP(i) = TP;
+    Sum_FP(i) = FP;
+    Sum_FN(i) = FN;
+    Sum_Jaccard(i) = Jaccard;
+    Sum_Euclidean(i) = Mean_Eu;
     
     if(TP > Max_TP)
         Max_TP = TP;
@@ -36,14 +40,22 @@ for i = 1: length(Vetores_de_Imagens{3})
     if(Jaccard > Max_Jaccard)
       Max_Jaccard = Jaccard;
     end
+     if(Max_Eu > Max_Euclidean)
+      Max_Euclidean = Max_Eu;
+    end
 end
 
-Mean_TP = (Sum_TP / length(Vetores_de_Imagens{3}));
-Mean_FN = (Sum_FN / length(Vetores_de_Imagens{3}));
-Mean_FP = (Sum_FP / length(Vetores_de_Imagens{3}));
-Mean_Jaccard = (Sum_Jaccard / length(Vetores_de_Imagens{3}))*100;
-Max_Jaccard = Max_Jaccard * 100;
+Mean_TP = mean(Sum_TP,"all");
+STD_TP = std(Sum_TP);
+Mean_FN = mean(Sum_FN, "all");
+STD_FN = std(Sum_FN);
+Mean_FP = mean(Sum_FP, "all");
+STD_FP = std(Sum_FP);
+Mean_Jaccard = mean(Sum_Jaccard, "all");
+STD_Jaccard = std(Sum_Jaccard);
+Mean_Euclidean = mean(Sum_Euclidean, "all");
+STD_Euclidean = std(Sum_Euclidean);
 
-Metrics_Table = table(Mean_TP, Max_TP, Mean_FN, Max_FN, Mean_FP, Max_FP, Mean_Jaccard, Max_Jaccard);
+Metrics_Table = table(Mean_TP, STD_TP, Max_TP, Mean_FN, STD_FN, Max_FN, Mean_FP, STD_FP, Max_FP, Mean_Jaccard, STD_Jaccard, Max_Jaccard, Mean_Euclidean, STD_Euclidean, Max_Euclidean);
 
 end
