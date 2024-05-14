0a2806faff595ec465d43fd053ca07a2768a4e3d
mariarocha2310 <167978174+mariarocha2310@users.noreply.github.com>
2024-05-14 16:46:01 +0100

    Add files via upload

--- evaluation_table.m
+++ evaluation_table.m
@@ -1,15 +1,28 @@
 function table = evaluation_table (evaluation)
+% Criação e representação de uma tabela com os valores médios, desvios e
+% máximos das grandezas a avaliar
+
+% Cálculo dos valores médios
 average_evaluation = zeros (1,6);
 for n=1:6
     average_evaluation(1,n) = mean(evaluation(:,n));
 end
+
+% Cálculo dos desvios padrão
 std_evaluation = zeros(1,6);
 for n=1:6
     std_evaluation(1,n) = std(evaluation(:,n));
 end
 
-table = [average_evaluation(1), average_evaluation(2), average_evaluation(3), average_evaluation(4), average_evaluation(5), average_evaluation(6); std_evaluation(1),std_evaluation(2), std_evaluation(3), std_evaluation(4), std_evaluation(5), std_evaluation(6)];
+% Cálculo dos valores máximos
+max_evaluation = zeros(1,6);
+for n=1:6
+    max_evaluation(1,n) = max(evaluation(:,n));
+end
+
+% Criação da tabela e representação na command window
+table = [average_evaluation(1), average_evaluation(2), average_evaluation(3), average_evaluation(4), average_evaluation(5), average_evaluation(6); std_evaluation(1),std_evaluation(2), std_evaluation(3), std_evaluation(4), std_evaluation(5), std_evaluation(6); max_evaluation(1),max_evaluation(2), max_evaluation(3), max_evaluation(4), max_evaluation(5), max_evaluation(6)];
 table = array2table(table);
 table.Properties.VariableNames = {'TP', 'FP', 'FN', 'recall', 'precision', 'F_measure'};
-table.Properties.RowNames = {'average', 'standart desviation'};
+table.Properties.RowNames = {'average', 'standart desviation', 'maximum'};
 end
